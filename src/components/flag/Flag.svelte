cdaf10b5c9a838fc6d3a1a03e2f64b3e4267cfde
Jason Raimondi <jason@raimondi.us>
2023-06-23 23:50:27 -0400

    fix: svg repeat

--- src/components/flag/Flag.svelte
+++ src/components/flag/Flag.svelte
@@ -2,4 +2,4 @@
 	import flag from './flag.svg';
 </script>
 
-<div class="w-[80px] h-[50px] rounded-lg" style="background-image: url({flag});">&nbsp;</div>
+<div class="w-[80px] h-[50px] rounded-lg bg-no-repeat" style="background-image: url({flag});">&nbsp;</div>
